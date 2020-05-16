e99a456ce1b00a2eb074949e3c89433ee7455bd8
Koushik Karmakar <koushik.karmakar@tum.de>
2020-05-16 03:44:22 +0200

    deleted stuff

--- i2dl_exercises/exercise_04_cleaned/exercise_code/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Define tests, sanity checks, and evaluation"""
-
-from .image_folder_dataset_tests import test_image_folder_dataset
-from .transform_tests import (
-    test_rescale_transform,
-    test_compute_image_mean_and_std
-)
-from .dataloader_tests import test_dataloader
-from .eval_utils import save_pickle
